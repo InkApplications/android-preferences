da0c605ff945b6fb8484e10c9f91313596321c8b
Andrew Reitz <aj.reitz@gmail.com>
2014-08-23 15:36:05 -0500

    Updated gradle stuff to the latest version.

--- preferences/src/test/java/com/inkapplications/preferences/StringPreferenceTest.java
+++ preferences/src/test/java/com/inkapplications/preferences/StringPreferenceTest.java
@@ -24,9 +24,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
+@Config(emulateSdk=18)
 @RunWith(RobolectricTestRunner.class)
 public class StringPreferenceTest {
 
